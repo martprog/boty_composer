616be9fd59dbf38d3b41576d0f4b31497210e73c
Martín Sotelo <martinscaraguel@gmail.com>
2022-09-01 18:50:14 +0200

    retouch

--- django_react/settings.py
+++ django_react/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import secretos
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tdvupor31va55mx_x8=c9bih!p&bs%76%&!w1-0zy^^6v0!rkp'
+SECRET_KEY = secretos.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
